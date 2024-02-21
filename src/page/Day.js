8a221cc554194140273a5694e281fa843308243a
antoalex07 <antoalex710@gmail.com>
2024-02-21 12:08:35 +0530

    designed home page and table components

--- src/page/Day.js
+++ src/page/Day.js
@@ -3,6 +3,10 @@ import { DataGrid } from '@mui/x-data-grid';
 import './Day.css';
 import api from '../api/axiosConfig';
 
+const handleButtonClick = (id) => {
+  console.log('Button clicked for row Id: ', id);
+};
+
 const columns = [
   { field: 'empId', 
     headerName: 'ID', 
@@ -30,30 +34,44 @@ const columns = [
     type: 'number',
     width: 90,
   },
+  {
+    field: 'actions',
+    headerName: 'Actions',
+    width: 150,
+    renderCell: (params) => (
+      <div>
+        <button onClick={() => handleButtonClick(params.row.empId)}>
+          Edit
+        </button>
+      </div>
+    ), 
+  },
 ];
 
-// const rows = [
-//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ];
+const rows = [
+  { id: 1, name: 'Snow', firstName: 'Jon', wageRate: 35 },
+  { id: 2, name: 'Lannister', firstName: 'Cersei', wageRate: 42 },
+  { id: 3, name: 'Lannister', firstName: 'Jaime', wageRate: 45 },
+  { id: 4, name: 'Stark', firstName: 'Arya', wageRate: 16 },
+  { id: 5, name: 'Targaryen', firstName: 'Daenerys', wageRate: null },
+  { id: 6, name: 'Melisandre', firstName: null, wageRate: 150 },
+  { id: 7, name: 'Clifford', firstName: 'Ferrara', wageRate: 44 },
+  { id: 8, name: 'Frances', firstName: 'Rossini', wageRate: 36 },
+  { id: 9, name: 'Roxie', firstName: 'Harvey', wageRate: 65 },
+];
 
 const Day = () => {
 
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const date = urlParams.get("date");
+  const urlParams = new URLSearchParams(window.location.search);
+  const date = urlParams.get("date");
+
   const [employees, setEmployees] = useState();
 
   const getEmployees = async () => {
+    
     try {
       
-      const response = await api.get("/api/v2/employee");
+      const response = await api.get(`/api/v2/employee/${date}`);
       console.log(response);
       setEmployees(response.data);
 
@@ -71,7 +89,7 @@ const Day = () => {
   return (
     <div className='table'>
       <DataGrid
-        rows={employees}
+        rows={rows}
         columns={columns}
         initialState={{
           pagination: {
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import Calendar from '../component/calendar/Calendar'
+import { Link } from 'react-router-dom'
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-        <Calendar/>
+    <div className='home'>
+      <div className='section section-1'>
+        <Link to="/page1" className='link'>
+          Section 1
+        </Link>
+      </div>
+      <div className='section section-2'>
+        <Link to="/page2" className='link'>
+          Section 2
+        </Link>
+      </div>
+      <div className='section section-3'>
+        <Link to="/page3" className='link'>
+          Section 3
+        </Link>
+      </div>
     </div>
   )
 }
