33a153ee8801641be4cc4ca4adb3ec212b63718a
antoalex07 <antoalex710@gmail.com>
2024-03-17 18:43:48 +0530

    ngrok path changed

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://2fbd-117-207-233-110.ngrok-free.app',
+    baseURL: 'https://db65-59-89-210-173.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
