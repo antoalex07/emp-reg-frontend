96fa12cd73a878cf96ed05929ad9be1451ce180a
antoalex07 <antoalex710@gmail.com>
2024-03-20 12:28:27 +0530

    form created for updating

--- src/page/updateAttendance/UpdateAttendance.js
+++ src/page/updateAttendance/UpdateAttendance.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../api/axiosConfig.js';
+import { Button, TextField } from '@mui/material';
 
 const UpdateAttendance = () => {
-  return (
-    <div>UpdateAttendance</div>
-  )
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    const [attendance, setAttendance] = useState(null);
+
+    const handleChange = () => {
+        console.log("murica");
+    }
+
+    const handleSubmit = () => {
+        console.log("canada");
+    }
+
+    // const getAttendance = async () => {
+    //     try {
+            
+    //         const response = await api.get(`/api/v2/attendance/${id}`);
+    //         console.log(response.status);
+    //         setAttendance(response.data);
+
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     getAttendance();
+    // }, [])
+
+    return (
+        <div className='form-container'>
+            <h1 className='heading'>Edit Attendance Details</h1>
+            <form className='form' onSubmit={handleSubmit}>
+
+                <div className='form-field'>
+                    <TextField
+                        id='outlined-read-only-input'
+                        label='Date'
+                        value={attendance.date}
+                        InputProps={{
+                            readOnly: true,
+                          }}
+                        sx={{width: 300}}
+                    />
+                </div>
+
+                <div className='form-field'>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Work"
+                        type="number"
+                        value={attendance.work}
+                        name="work"
+                        onChange={handleChange}
+                        sx={{width: 300}}
+                    />
+                </div>
+
+                <div className='form-field'>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Overtime"
+                        type="number"
+                        value={attendance.overtime}
+                        name="overtime"
+                        onChange={handleChange}
+                        sx={{width: 300}}
+                    />
+                </div>
+          
+                <div className='form-field'>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Advance"
+                        type="number"
+                        value={attendance.advance}
+                        name="advance"
+                        onChange={handleChange}
+                        sx={{width: 300}}
+                    />
+                </div>
+          
+          <Button variant="contained" type='submit'>Submit</Button>
+
+            </form>
+        </div>
+    )
 }
 
 export default UpdateAttendance
