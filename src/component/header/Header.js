a447ebfb3e0b8e08211af21e73070afc303e8d19
antoalex07 <antoalex710@gmail.com>
2024-04-23 18:03:36 +0530

    Header components reduced

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,37 +1,16 @@
-import { faMeteor } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Button, Container, Nav, Navbar } from 'react-bootstrap'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import "./Header.css";
 
 const Header = () => {
 
-    const navigate = useNavigate();
-
-    const handleButtonClick = () => {
-        navigate("/employeeRegister");
-    }
 
   return (
-    <Navbar bg='dark' variant='dark' expand="lg" fixed='top'>
-        <Container fluid>
-            <Navbar.Brand href='/' style={{"color":'gold'}}>
-                <FontAwesomeIcon icon={faMeteor} /> VJ Constructions
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls='navbarScroll' />
-            <Navbar.Collapse id="navbarScroll">
-                <Nav
-                    className='me-auto my-2 my-lg-0'
-                    style={{maxHeight: '100px'}}
-                    navbarScroll
-                >
-                    <NavLink className="nav-link" to="/"></NavLink>
-                    <NavLink className="nav-link" to="/"></NavLink>
-                </Nav>
-                <Button variant='outline-info' onClick={handleButtonClick}>Employee Register</Button>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
+    <nav>
+        <div className='nav-logo-container'>
+            <a className="link" href="/">VJ CONSTRUCTIONS</a>
+        </div>
+    </nav>
+
   )
 }
 
