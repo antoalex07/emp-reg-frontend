dd7d09ca3168a50a8d41fe9bafc1da595aea6824
antoalex07 <antoalex710@gmail.com>
2024-05-01 23:16:55 +0530

    desgin changes made to fit user experience

--- src/page/updateAttendance/UpdateAttendance.js
+++ src/page/updateAttendance/UpdateAttendance.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../api/axiosConfig.js';
-import { Button, TextField } from '@mui/material';
+import { Button, IconButton, InputBase, Paper, TextField } from '@mui/material';
+import './UpdateAttendance.css';
+import { KeyboardBackspaceRounded } from '@mui/icons-material';
+import TodayRoundedIcon from '@mui/icons-material/TodayRounded';
 
 const UpdateAttendance = () => {
 
@@ -13,6 +16,12 @@ const UpdateAttendance = () => {
         overtime: 4,
         advance: 1000
     });
+
+    const handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            event.preventDefault();
+        }
+    }
 
     const handleChange = () => {
         console.log("murica");
@@ -39,64 +48,143 @@ const UpdateAttendance = () => {
     }, [])
 
     return (
-        <div className='form-container'>
-            <h1 className='heading'>Edit Attendance Details</h1>
-            <form className='form' onSubmit={handleSubmit}>
-
-                <div className='form-field'>
-                    <TextField
-                        id='outlined-read-only-input'
-                        label='Date'
-                        value={attendance.date}
-                        InputProps={{
-                            readOnly: true,
-                          }}
-                        sx={{width: 300}}
-                    />
+        <div className='page'>
+            <div className='form-container'>
+                
+                <div className='image-container'>
+                        <KeyboardBackspaceRounded 
+                            htmlColor='#818283'
+                            style={{ position: 'fixed',
+                                    top: '110px',
+                                    left: '231px',
+                                    fontSize: '40px',   
+                                }}/>
                 </div>
+                
+                <div className='edit-form'>
+                    
+                    <h1 className='heading'>Edit Attendance</h1>
 
-                <div className='form-field'>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Work"
-                        type="number"
-                        value={attendance.work}
-                        name="work"
-                        onChange={handleChange}
-                        sx={{width: 300}}
-                    />
-                </div>
+                    <Paper
+                        component="form"
+                        sx={{
+                            marginTop: '6%',
+                            marginLeft: '12px',
+                            p: '10px 4px',
+                            display: 'flex',
+                            width: 350,
+                            borderRadius: '30px',
+                            alignItems: 'center',
+                            backgroundColor: '#c2f3dd'
+                        }}
+                        >
+                        <TodayRoundedIcon sx={{ml: 1}} htmlColor='#818283' /> 
+                        <InputBase
+                            id='date-input'
+                            placeholder='Date (yyyy-mm-dd)'
+                            type='text'
+                            value={attendance.date}
+                            onKeyDown={handleKeyPress}
+                            inputProps={{
+                                "aria-readonly": 'true'
+                                }}
+                            sx={{ flex: 1, marginLeft: '10px', fontSize: '18px' }}
+                        />
+                    </Paper>
 
-                <div className='form-field'>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Overtime"
-                        type="number"
-                        value={attendance.overtime}
-                        name="overtime"
-                        onChange={handleChange}
-                        sx={{width: 300}}
-                    />
-                </div>
-          
-                <div className='form-field'>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Advance"
-                        type="number"
-                        value={attendance.advance}
-                        name="advance"
-                        onChange={handleChange}
-                        sx={{width: 300}}
-                    />
-                </div>
-          
-          <Button variant="contained" type='submit'>Submit</Button>
+                    <Paper
+                        component="form"
+                        sx={{
+                            marginTop: '12px',
+                            marginLeft: '12px',
+                            p: '10px 4px',
+                            display: 'flex',
+                            width: 350,
+                            borderRadius: '30px',
+                            alignItems: 'center',
+                            backgroundColor: '#c2f3dd'
+                        }}
+                        >
+                        <TodayRoundedIcon sx={{ml: 1}} htmlColor='#818283' /> 
+                        <InputBase
+                            id='work'
+                            sx={{ 
+                                flex: 1, 
+                                marginLeft: '10px', 
+                                fontSize: '18px',
+                                "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button": { display: "none" },
+                                "& input[type=number]": { MozAppearance: "inputbase" } 
+                            }}
+                            placeholder='Work'
+                            type='number'
+                            defaultValue={attendance.work}
+                            onKeyDown={handleKeyPress}
+                        />
+                    </Paper>
 
-            </form>
+                    <Paper
+                        component="form"
+                        sx={{
+                            marginTop: '12px',
+                            marginLeft: '12px',
+                            p: '10px 4px',
+                            display: 'flex',
+                            width: 350,
+                            borderRadius: '30px',
+                            alignItems: 'center',
+                            backgroundColor: '#c2f3dd'
+                        }}
+                        >
+                        <TodayRoundedIcon sx={{ml: 1}} htmlColor='#818283' /> 
+                        <InputBase
+                            id='overtime'
+                            sx={{ 
+                                flex: 1, 
+                                marginLeft: '10px', 
+                                fontSize: '18px',
+                                "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button": { display: "none" },
+                                "& input[type=number]": { MozAppearance: "inputbase" } 
+                            }}
+                            placeholder='Overtime Hours'
+                            type='number'
+                            defaultValue={attendance.overtime}
+                            onKeyDown={handleKeyPress}
+                        />
+                    </Paper>
+                    
+                    <Paper
+                        component="form"
+                        sx={{
+                            marginTop: '12px',
+                            marginLeft: '12px',
+                            p: '10px 4px',
+                            display: 'flex',
+                            width: 350,
+                            borderRadius: '30px',
+                            alignItems: 'center',
+                            backgroundColor: '#c2f3dd'
+                        }}
+                        >
+                        <TodayRoundedIcon sx={{ml: 1}} htmlColor='#818283' /> 
+                        <InputBase
+                            id='advance'
+                            sx={{ 
+                                flex: 1, 
+                                marginLeft: '10px', 
+                                fontSize: '18px',
+                                "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button": { display: "none" },
+                                "& input[type=number]": { MozAppearance: "inputbase" } 
+                            }}
+                            placeholder='Advance'
+                            type='number'
+                            defaultValue={attendance.advance}
+                            onKeyDown={handleKeyPress}
+                        />
+                    </Paper>
+
+                </div>
+            
+            </div>
         </div>
     )
 }
